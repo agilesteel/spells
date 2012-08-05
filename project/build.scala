ba206239c871bb3640b1566be124eed767623476
Vladyslav Pekker <agilesteel@gmail.com>
2012-08-05 15:25:39 +0200

    Added homepage, startYear, description and licenses to build.scala

--- project/build.scala
+++ project/build.scala
@@ -7,7 +7,11 @@ object SpellsBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.github.agilesteel",
     version := "1.1",
-    scalaVersion := "2.9.2")
+    scalaVersion := "2.9.2",
+    homepage := Some(url("http://agilesteel.github.com/spells/")),
+    startYear := some(2012),
+    description := """This is a small scala "util" library, which will hopefully grow over time.""",
+    licenses += "Apache 2" -> url("http://www.apache.org/licenses/LICENSE-2.0"))
 
   override lazy val settings = super.settings ++ buildSettings
 
