7fdddcc1c2bfd43271386d25cc3d86e12e0fb54e
Vladyslav Pekker <agilesteel@gmail.com>
2015-11-20 22:18:01 +0100

    Added FunctionNOpsTests.

---
+++ src/test/scala/spells/user/FunctionNOpsTests.scala
@@ -0,0 +1,28 @@
+package spells.user
+
+class FunctionNTests extends spells.UnitTestConfiguration {
+  test("FunctionN rendering tests") {
+    ((_: Int) => "").rendered should be("Int => java.lang.String")
+    ((_: Int, _: Int) => "").rendered should be("(Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+    ((_: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int, _: Int) => "").rendered should be("(Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int, Int) => java.lang.String")
+  }
+}
